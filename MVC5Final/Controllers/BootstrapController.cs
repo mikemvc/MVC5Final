959e551da59ca7f0a270d571fc52b53e4d970f2b
mikemvc@hotmail.com <mikemvc@hotmail.com>
2014-12-15 23:36:54 +0800

    v6

---
+++ MVC5Final/Controllers/BootstrapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Final.Controllers
+{
+    public class BootstrapController : Controller
+    {
+        // GET: Bootstrap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
