610c7d3a20c2ccf9dff06a1090a338c15d8e22fd
mikemvc@hotmail.com <mikemvc@hotmail.com>
2014-12-15 23:26:15 +0800

    V1

---
+++ MVC5Final/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Final.Startup))]
+namespace MVC5Final
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
